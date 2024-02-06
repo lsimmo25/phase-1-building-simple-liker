126223fa9f53c170b841ab7f099b62fadcbfa73e
Lyle Simmons <lylesimmons125@gmail.com>
2024-02-05 20:29:21 -0500

    lab complete

--- main.js
+++ main.js
@@ -4,7 +4,42 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+//handleFetch Function
 
+function handleFetch() {
+  const likeButtons = Array.from(document.querySelectorAll(".like-glyph"));
+
+  likeButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+      if (e.target.textContent === EMPTY_HEART) {
+        e.target.textContent = FULL_HEART;
+        e.target.classList.remove("like-glyph");
+        e.target.classList.add("activated-heart");
+
+        mimicServerCall()
+          .then(response => {
+            // Success
+            console.log(response);
+          })
+          .catch(error => {
+            // Error
+            let errorMessage = document.querySelector("#modal")
+            errorMessage.classList.remove("hidden")
+            setTimeout(() => {errorMessage.classList.add("hidden")}, 3000)
+
+            console.error(error);
+          });
+
+      } else {
+        e.target.textContent = EMPTY_HEART;
+        e.target.classList.remove("activated-heart");
+        e.target.classList.add("like-glyph");
+      }
+    });
+  });
+}
+
+handleFetch();
 
 
 //------------------------------------------------------------------------------
